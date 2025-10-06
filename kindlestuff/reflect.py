1b502f7b91f2f025d4ddb0ff9c88b616173220d6
Tam <alto.dizi@gmail.com>
2025-10-06 13:43:04 -0700

    Update reflect.py
    Added ability to specify filename

--- kindlestuff/reflect.py
+++ kindlestuff/reflect.py
@@ -4,16 +4,22 @@
 # note you have to replace the host with the zero's reported host, and I haven't tested in the field (ie, using a phone hotspot or something) at all
 # also it's slow and good luck if you lose focus
 
+import sys
 from flask import Flask, render_template
+
+if len(sys.argv) == 2: # filename specified hopefully
+    fname = sys.argv[1]
+else:
+    fname = "scribble.md"
 
 app = Flask(__name__, template_folder="")
 
 @app.route('/', methods=["GET", "POST"])
 def index():
     #b_lines = [row for row in reversed(list(open("app.py")))]
-    b_lines = [row for row in list(open("scratchdoc.md"))]
-    # if I could figure out how to pull from the geany buffer directly I could just pipe it over
+    b_lines = [row for row in list(open(fname))]
+    # if I could figure out how to pull from the buffer directly I could just pipe it over
     return render_template('index.html', b_lines=b_lines)
 
 if __name__ == "__main__":
-    app.run(host='192.168.7.XXX', port=5000, debug=True, threaded=False)
+    app.run(host='192.168.7.176', port=5000, debug=True, threaded=False)
